107ecbb9daa8df2e35c3451d986d0ad01bc6e582
[dyad] <git@dyad.sh>
2025-07-03 22:00:34 -0700

    [dyad] Cập nhật màu nền trang web - wrote 1 file(s)

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -1,16 +1,17 @@
-import React from "react";
-import Sidebar from "./Sidebar";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
-const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr] bg-[#0F0F1A]">
-      <div className="hidden border-r border-gray-800 bg-[#1C1C22] md:block">
-        <Sidebar />
-      </div>
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr] bg-zinc-950">
+      <Sidebar />
       <div className="flex flex-col">
         <Header />
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-[#2A214D] md:rounded-tl-3xl">
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-zinc-900 md:rounded-tl-3xl">
           {children}
         </main>
       </div>
